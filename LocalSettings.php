29827096a7cef10abfacc8e4db53523b1580e05c
skp098 <skp09098@gmail.com>
2022-09-27 22:14:36 +0530

    added extension

--- LocalSettings.php
+++ LocalSettings.php
@@ -128,16 +128,18 @@ $wgDiff3 = "/usr/bin/diff3";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "vector-2022";
 
 # Enabled skins.
 # The following skins were automatically enabled:
-wfLoadSkin( 'MinervaNeue' );
-wfLoadSkin( 'MonoBook' );
-wfLoadSkin( 'Timeless' );
 wfLoadSkin( 'Vector' );
 
 
 # End of automatically generated settings.
 # Add more configuration options below.
 
+#adding extensions
+wfLoadExtension( 'Cargo' );
+wfLoadExtension( 'mediawiki-extensions-PageForms' );
+wfLoadExtension( 'Upvote' );
+wfLoadExtension( 'Comments' );
