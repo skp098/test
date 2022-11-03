413d8db7127121585562458bbf319319f598a54f
Santosh Kumar <santosh.k@smarter.codes>
2022-11-03 07:25:24 +0000

    test pipeline

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -199,3 +199,5 @@ $wgReCaptchaSiteKey = loadenv('GCAPTCHA_KEY', "6LePJ8kiAAAAAFE_q8ras0ljCFxpwwUy_
 $wgReCaptchaSecretKey = loadenv('GCAPTCHA_SECRET', "6LePJ8kiAAAAANe82zeJgpRNEO6bW1epc4mLCzwk");
 
 $wgCaptchaTriggers['createaccount'] = true;
+
+//test cicd
