72258a043470323eff9f40b135a7a66637a0a3b0
skp098 <santosh.k@smarter.codes>
2023-02-10 09:23:29 +0530

    Reverting back MobileRes due to error

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -315,5 +315,5 @@ $wgOAuth2Client['configuration']['email'] = 'email'; // JSON path to email
 // $wgOAuth2Client['configuration']['scopes'] = 'username'; //Permissions
 
 //making oe mobile friendly
-wfLoadExtension( 'MobileFrontend' );
-$wgDefaultMobileSkin = 'vector-2022';
+// wfLoadExtension( 'MobileFrontend' );
+// $wgDefaultMobileSkin = 'vector-2022';
